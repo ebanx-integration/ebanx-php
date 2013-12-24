7e6b10aeb8a136e3d4825b62aacbf83dd06c8458
Gustavo Henrique Mascarenhas Machado <guhemama@gmail.com>
2013-12-24 08:16:12 -0200

    Added autoloader

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
 \Ebanx\Config::set(array(
   'integrationKey' => '',
   'directMode'     => false,
--- examples/exchange.php
+++ examples/exchange.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
 require_once 'bootstrap.php';
 
 $response = \Ebanx\Ebanx::doExchange(array('currency_code' => 'USD'));
