1ee99db3fa77bd06897677a0897d5eb20de6118d
Gustavo Henrique Mascarenhas Machado <guhemama@gmail.com>
2015-05-07 10:50:44 -0300

    Fixed  bootstrap

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../src/autoload.php';
 
 \Ebanx\Config::set(array(
-  'integrationKey' => '1231000',
+  'integrationKey' => 'b9fa8b1d4231889a1445e1ebfdfa2a3558ef3cc651d523eb1b12cdf8af3afc518f17b36edf5e8cfc59a8338bb9241aacd427',
   'directMode'     => false,
   'testMode'       => true
 ));
